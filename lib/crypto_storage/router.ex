e57b4a2ee28a35a64f550f3ccbc27d3f54a31025
odelbos <od@phibox.com>
2022-02-24 13:20:58 +0100

    feat: add temporary route to output the config

--- lib/crypto_storage/router.ex
+++ lib/crypto_storage/router.ex
@@ -2,6 +2,8 @@ defmodule CryptoStorage.Router do
 
   use Plug.Router
 
+  alias CryptoStorage.ConfigKV
+
   plug :match
   plug :dispatch
 
@@ -9,5 +11,18 @@ defmodule CryptoStorage.Router do
     conn
       |> put_resp_content_type("text/plain")
       |> send_resp(200, "Welcome to Crypto Storage!")
+  end
+
+  # TODO : Temporary route to test ConfigKV
+  # (it will be removed soon)
+  get "/config" do
+    content = ""
+      |> Kernel.<>("read size   : #{ConfigKV.get :read_size}\n")
+      |> Kernel.<>("blocksize   : #{ConfigKV.get :block_size}\n")
+      |> Kernel.<>("blocks path : #{ConfigKV.get :blocks_path}\n")
+      |> Kernel.<>("files path  : #{ConfigKV.get :files_path}\n")
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, content)
   end
 end
