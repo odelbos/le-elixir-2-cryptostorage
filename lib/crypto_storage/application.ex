708dd292435c88dd69c0bfd97c8acc4432c28369
odelbos <od@phibox.com>
2022-02-24 13:19:49 +0100

    feat: add ConfigKV to the application supervisor

--- lib/crypto_storage/application.ex
+++ lib/crypto_storage/application.ex
@@ -1,6 +1,4 @@
 defmodule CryptoStorage.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,13 +6,10 @@ defmodule CryptoStorage.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: CryptoStorage.Worker.start_link(arg)
-      # {CryptoStorage.Worker, arg}
-      {Plug.Cowboy, scheme: :http, plug: CryptoStorage.Router, options: [port: 4554]}
+      {Plug.Cowboy, scheme: :http, plug: CryptoStorage.Router, options: [port: 4554]},
+      {CryptoStorage.ConfigKV, []}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: CryptoStorage.Supervisor]
     Supervisor.start_link(children, opts)
   end
