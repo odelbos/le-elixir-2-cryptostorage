2f9ddd5e6bb6e2667a45986fdba4f5356c4a9914
odelbos <od@phibox.com>
2022-03-08 23:08:00 +0100

    feat: update comments

--- lib/crypto_storage/router.ex
+++ lib/crypto_storage/router.ex
@@ -5,6 +5,7 @@ defmodule CryptoStorage.Router do
   alias CryptoStorage.Plug.VerifySetup
   alias CryptoStorage.Settings
   alias CryptoStorage.Utils
+  alias CryptoBlocks.Crypto
 
   plug VerifySetup
   plug :match
@@ -31,12 +32,11 @@ defmodule CryptoStorage.Router do
 
   post "/setup" do
     {:ok, payload, new_conn} = read_body conn
-
     try do
       [b64_storage_key, b64_time, b64_sig] = String.split payload, "."
       sig = Base.decode64! b64_sig, padding: false
 
-      # Get public key
+      # Get RSA public key
       der_pub_key = Settings.get(:pub_key) |> Base.decode64!(padding: false)
       pub_key = :public_key.der_decode :RSAPublicKey, der_pub_key
 
@@ -49,9 +49,7 @@ defmodule CryptoStorage.Router do
           raise "Bad timing"
         end
 
-        # Decrypt and store the storage key in config
-        # e_storage_key = Base.decode64! b64_storage_key, padding: false
-        # storage_key = :public_key.decrypt_public e_storage_key, pub_key
+        # Decrypt and store the storage key in settings
         storage_key = Base.decode64!(b64_storage_key, padding: false)
           |> :public_key.decrypt_public(pub_key)
 
@@ -62,7 +60,7 @@ defmodule CryptoStorage.Router do
           |> put_resp_content_type("text/plain")
           |> send_resp(:ok, "ok")
       else
-        Logger.error "Setup error - (bad signature)"
+        Logger.error "Error - Bad signature"
         new_conn
           |> put_resp_content_type("text/plain")
           |> send_resp(:unprocessable_entity, "Unprocessable Entity")
@@ -77,24 +75,35 @@ defmodule CryptoStorage.Router do
   end
 
   get "/id/:id" do
-    # Read the blocks description
-    files_path = Settings.get :files_path
-    filepath = Path.join [files_path, id]
+    try do
+      # Read blocks description
+      files_path = Settings.get :files_path
+      filepath = Path.join [files_path, id]
 
-    if File.exists? filepath do
-      {:ok, data} = File.read filepath
-      # Unpack blocks
-      blocks = CryptoBlocks.Utils.unpack data
+      if File.exists? filepath do
+        data = File.read! filepath
+        <<iv::128, tag::128, encrypted_blocks::binary>> = data
+        iv = <<iv::128>>
+        tag = <<tag::128>>
+        # Decrypt and unpack blocks
+        blocks = Crypto.decrypt(encrypted_blocks, Settings.get(:storage_key), iv, tag)
+          |> CryptoBlocks.Utils.unpack()
 
-      new_conn = conn
-        |> put_resp_header("Content-Type", "application/octet-stream")
-        |> put_resp_header("Content-Disposition","attachment; filename=\"#{id}\"")
-        |> send_chunked(200)
-
-      send_blocks new_conn, blocks
-    else
-      conn
-        |> send_resp(404, "404 Not Found")
+        new_conn = conn
+          |> put_resp_header("Content-Type", "application/octet-stream")
+          |> put_resp_header("Content-Disposition","attachment; filename=\"#{id}\"")
+          |> send_chunked(200)
+        send_blocks new_conn, blocks
+      else
+        conn
+          |> send_resp(404, "404 Not Found")
+      end
+    rescue
+      _e ->
+        Logger.error "Cannot get file"
+        conn
+          |> put_resp_content_type("text/plain")
+          |> send_resp(:unprocessable_entity, "Unprocessable Entity")
     end
   end
 
@@ -103,7 +112,7 @@ defmodule CryptoStorage.Router do
     [value | _] = get_req_header conn, "content-length"
     content_length = String.to_integer value
     block_size = Utils.compute_block_size content_length
-    # Get some config values
+    # Get some setting values
     blocks_path = Settings.get :blocks_path
     files_path = Settings.get :files_path
 
@@ -130,13 +139,15 @@ defmodule CryptoStorage.Router do
     # TODO : if blocks_hash != hash
     #
 
-    # Pack the blocks description
+    # Pack and encryptthe blocks description
     packed_blocks = CryptoBlocks.Utils.pack blocks
+    {iv, tag, encrypted_blocks} =
+      Crypto.encrypt packed_blocks, Settings.get(:storage_key)
 
     # Write blocks description to disk
     id = generate_id files_path
     filepath = Path.join [files_path, id]
-    File.write filepath, packed_blocks, [:binary, :raw]
+    File.write filepath, iv <> tag <> encrypted_blocks, [:binary, :raw]
 
     Logger.info "Post file : #{id}"
 
